3d0c7648f693afafdea55c678d0e5be1f585e76b
Nguyen Thi Hao <haonguyen4993@gmail.com>
2018-05-28 21:49:55 +0700

    User view booking history

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,14 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+
+  get "/history", to: "histories#index"
+
+  resources :tours, only: :show
+  resources :users, only: :show
+
   namespace :admin do
     root "dashboard#index"
     resources :bookings, only: %i(index show update)
   end
-  resources :tours, only: :show
 end
