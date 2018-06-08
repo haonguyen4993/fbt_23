1f46982ab253c135eaa3290aebea2f77bfbe7ae8
Nguyen Thi Hao <haonguyen4993@gmail.com>
2018-06-08 08:46:55 +0700

    login logout using devise

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,8 +1,8 @@
 class HistoriesController < ApplicationController
-  before_action :logged_in_user
+  before_action :authenticate_user!
 
   def index
-    @bookings = @current_user.bookings.created_at_sort
+    @bookings = current_user.bookings.created_at_sort
       .paginate page: params[:page], per_page: Settings.booking.per_page
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180604143312) do
+ActiveRecord::Schema.define(version: 20180607091453) do
 
   create_table "bookings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "status",                           default: 0
@@ -102,13 +102,18 @@ ActiveRecord::Schema.define(version: 20180604143312) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
-    t.string   "email"
-    t.string   "password_digest"
     t.string   "address"
     t.string   "phone_number"
     t.integer  "role"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "bookings", "description_details"
